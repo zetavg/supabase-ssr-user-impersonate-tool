42e066a6e8c9687089d2c64979a1de63d2ef3fa1
Pokai Chang <mail@zeta.vg>
2025-05-20 09:56:45 +0800

    check email to prevent typo

--- main.mjs
+++ main.mjs
@@ -15,6 +15,14 @@ const main = defineCommand({
       description: 'Email address of the user to impersonate.',
       required: true,
     },
+    'skip-check': {
+      description: 'Bypass the user existence check before proceeding. A new account will be created if a user with the given email does not exist.',
+      required: false,
+    },
+    'dry-run': {
+      description: 'Skip generating a login request.',
+      required: false,
+    },
   },
   async run({ args }) {
     const { email } = args
@@ -28,15 +36,43 @@ const main = defineCommand({
       process.exit(1)
     }
 
-
-    // Step 1: Generate a magic link
-
     const supabase = createClient(supabase_url, service_role_key, {
       auth: {
         autoRefreshToken: false,
         persistSession: false
       }
     })
+
+
+    // Step 0: Check if the user exists
+
+    if (!args['skip-check']) {
+      const { data: usersData, error: userError } = await supabase.from('users')
+        .select('*')
+        .eq('email', email);
+
+      if (userError) {
+        console.error('Error checking user existence:', userError)
+        console.error('If you do not have a `users` table under the public schema with an `email` column, please use the --skip-check flag to bypass this check.')
+        process.exit(1)
+      }
+
+      const user = usersData[0]
+      if (!user) {
+        console.error(`User with email ${email} does not exist. Use --skip-check to bypass this check and let a new account be created automatically with the given email.`)
+        process.exit(1)
+      }
+
+      console.log(`// Email: ${email}`)
+      console.log(`// User ID: ${user.id}`)
+    }
+
+    if (args['dry-run']) {
+      process.exit(0)
+    }
+
+
+    // Step 1: Generate a magic link
 
     const { data, error: generateLinkError } = await supabase.auth.admin.generateLink({
       type: 'magiclink',
@@ -84,11 +120,15 @@ const main = defineCommand({
       process.exit(1)
     }
 
-console.log('// Please execute the following JS snippet in the site, and refresh the page to sign in as the user:')
-console.log('')
-console.log('// --------')
-console.log('')
-console.log(`
+    const jsSnippetInstructions = [
+      '// Please execute this JS snippet above on the site and refresh the page.',
+      '// You may need to manually clear the cookies in your browser before executing this snippet if it did not work.',
+    ].join('\n')
+
+    console.log('')
+    console.log(jsSnippetInstructions)
+    console.log('')
+    console.log(`
 (() => {
   const cookies = ${JSON.stringify(cookies)}
 
@@ -120,9 +160,9 @@ console.log(`
 
   writeCookies(cookies)
 })()
-`)
-console.log('')
-console.log('// --------')
+`.trim())
+    console.log('')
+    console.log(jsSnippetInstructions)
   },
 })
 
